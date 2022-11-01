017499435a59853d54e8380d626187806a1bd21a
ChanlderQ <92227847+ChanlderQ@users.noreply.github.com>
2022-11-01 18:53:05 -0400

    add vido support

--- fire_detection_ui.py
+++ fire_detection_ui.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import pandas as pd
-import cv2,argparse
+import cv2,argparse,os,sys
+from yolov5 import detect
+from PIL import Image
+from pathlib import Path
+import ffmpeg
 
 def welcome():
     st.title("Fire Detection")
@@ -11,18 +15,50 @@ def welcome():
     show_file2.image("example2.jpg")
 
 def upload_image():
-    upload_file=st.file_uploader("please upload the picture",type=["jpg","png","jpeg"])
+    upload_file=st.file_uploader("please upload the picture",type=["jpg","png","jpeg","mp4"])
     if upload_file is not None:
         show_file=st.empty()
         show_file.image(upload_file)
+        picture = Image.open(upload_file)
+        picture.save(f'data/{upload_file.name}')
     return upload_file
 
+def upload_video():
+    upload_file=st.file_uploader("please upload the video",type=["mp4","mov"])
+    if upload_file is not None:
+        #show_file=st.empty()
+        #show_file.video(upload_file)
+        with open(f'data/{upload_file.name}', "wb") as f:
+            f.write(upload_file.getbuffer())
+    return upload_file
+
+def transform_video(input_file_path,output_file_path):
+    stream=ffmpeg.input(input_file_path)
+    stream=ffmpeg.output(stream,output_file_path)
+    ffmpeg.run(stream)
+
+def get_subdirs(b='.'):
+    '''
+        Returns all sub-directories in a specific Path
+    '''
+    result = []
+    for d in os.listdir(b):
+        bd = os.path.join(b, d)
+        if os.path.isdir(bd):
+            result.append(bd)
+    return result
+    
+def get_detection_folder():
+    '''
+        Returns the latest folder in a runs\detect
+    '''
+    return max(get_subdirs(os.path.join('runs', 'detect')), key=os.path.getmtime)
 
 
 def main():
     selected_box=st.sidebar.selectbox(
         "Choose one of the following",
-        ("Welcome","Subscription List","Upload Image")
+        ("Welcome","Subscription List","Upload Image","Upload Video")
     )
     
     submit_button=st.sidebar.button("Fire Detection")
@@ -32,7 +68,61 @@ def main():
     if selected_box=="Subscription List":
         pass
     if selected_box=="Upload Image":
-        upload_image()
+        upload_file=upload_image()
+
+        if submit_button:
+            parser = argparse.ArgumentParser()
+            parser.add_argument('--weights', nargs='+', type=str,
+                                    default='best.pt', help='model.pt path(s)')
+            parser.add_argument('--source', type=str,
+                                    default='', help='source')
+            
+            opt, unknown = parser.parse_known_args()
+            
+            opt.source=f'data/{upload_file.name}'
+            opt.project=''
+            opt.name=f'result/'
+            opt.exist_ok=True
+            opt.view_img=True
+            
+            
+            detect.main(opt)
+            path=f'./result/'
+            print(path)
+            for img in os.listdir(path):
+                display_img_filename=path+"/"+img
+                print(img)
+                st.image(display_img_filename)
+    
+    if selected_box=="Upload Video":
+        upload_file=upload_video()
+
+        if submit_button:
+            parser = argparse.ArgumentParser()
+            parser.add_argument('--weights', nargs='+', type=str,
+                                    default='best.pt', help='model.pt path(s)')
+            parser.add_argument('--source', type=str,
+                                    default='', help='source')
+            
+            opt, unknown = parser.parse_known_args()
+            
+            opt.source=f'data/{upload_file.name}'
+            opt.project=''
+            opt.name=f'result/'
+            opt.exist_ok=True
+            
+            with st.spinner(text="processing..."):
+                detect.main(opt)
+                path=f'./result/'
+                for img in os.listdir(path):
+                    display_img_filename=path+"/"+img
+                    print("output file :",str(Path(display_img_filename)))
+                    output_file_path="./output_result/output_video.mp4"
+                    transform_video(str(Path(display_img_filename)),output_file_path)
+                    video_file = open(output_file_path, 'rb')
+                    video_bytes = video_file.read()
+                    st.video(video_bytes)
+                    st.balloons()
     
 if __name__=="__main__":
     main()
